05efaa5e8002956816802c8f17ba0b4bd5aa78ad
Marten Hernebring <marten.hernebring@gmail.com>
2020-10-19 10:22:20 +0200

    add help command

--- Game.java
+++ Game.java
@@ -6,31 +6,47 @@ import java.io.FileNotFoundException;
 
 public class Game {
     public static void main(String[] args) {
-	System.out.println("Countries Game");
-	Console console = System.console();
-	BufferedReader br = null;
-	try {
-	    br = new BufferedReader(new FileReader("continents.csv"));
-	    String line;
-	    while((line=br.readLine()) != null) {
-		    String[] cols = line.split(",");
-		    String country = cols[0];
-		    String continent= cols[1];
-		    String answer= console.readLine("which continent does " + country+ " belong to?");
-	        if(answer.equals(continent)) {
-	            System.out.println("Correct!");
-	        }else {
-	        System.out.println("No, the answer is " + continent);
-	        }    
+        System.out.println("Countries Game");
+        System.out.println("Type 'exit' to stop the game");
+        Console console = System.console();
+        BufferedReader br = null;
+        int total = 0;
+        int correct = 0;
+
+        try {
+            br = new BufferedReader(new FileReader("continents.csv"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                total++;
+                String[] cols = line.split(",");
+                String country = cols[0];
+                String continent = cols[1];
+                String answer = null;
+                answer = console.readLine("which continent does " + country + " belong to?");
+                do {
+                    answer = console.readLine("Which continent does " + country + " belong to? ");
+                    if (answer.equalsIgnoreCase("help")) {
+                        printHelp();
+                    }
+                } while (answer.equalsIgnoreCase("help"));
+                if (answer.equalsIgnoreCase("exit")) {
+                    break;
+                } else if (answer.equals(continent)) {
+                    System.out.println("Correct!");
+                    correct++;
+                } else {
+                    System.out.println("No, the answer is " + continent);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-	}catch(FileNotFoundException e) {
-	    e.printStackTrace();
-	}catch(IOException e) {
-	    e.printStackTrace();
-	}
+        System.out.println("You scored  " + correct + " out of  " + total);
     }
 
-    public static void printHelp(){
+    public static void printHelp() {
         System.out.println("Guess the continent");
         System.out.println("Type 'exit' to stop the game or 'help' to get this text.");
     }
